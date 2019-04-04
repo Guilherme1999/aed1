0033ae550023713207489949c78ed39b8aa5ff34
Guilherme1999 <guiscaglioni@gmail.com>
2019-04-04 00:11:17 -0300

    modificações

--- agenda.c
+++ agenda.c
@@ -5,44 +5,53 @@ struct pessoa{
     char nome[20];
     int idade,mat;
     };
+void* buffer;
+struct pessoa* p,*p2;
+int* aux;
+ int* countp;
+ int* x;
+ int* i;
 
-/*void* alocapont(void* buffer, struct pessoa* p, int* aux, int* countp, int* x, int* i){
-    buffer = malloc(3*sizeof(int)+sizeof(struct pessoa));
+void alocapont(void){
+    buffer = malloc(4*sizeof(int)+sizeof(struct pessoa));
     aux=(int*)buffer;
     countp = (int*)aux + 1;
     x = (int*)countp + 1;
     i = (int*)x + 1;
     p = (struct pessoa*)i + 1;
-    return buffer;
-}*/
-void realocapont(void* buffer, struct pessoa* p, int* aux, int* countp, int* x, int* i){
-    //*countp += *aux;
-    buffer = realloc(buffer,(*countp)*sizeof(struct pessoa));
+    p2 = (struct pessoa*)i + 1;
+}
+void realocapont(void){
+    *countp += *i;
+    buffer = realloc(buffer,4*sizeof(int) + (*countp)*sizeof(struct pessoa));
     aux = (int*)buffer;
     countp = (int*)aux + 1;
     x = (int*)countp + 1;
     i = (int*)x + 1;
     p = (struct pessoa*)i + 1;
+    if((*countp)==(*i)) p2 = (struct pessoa*)i + 1;
+    else p2 = (struct pessoa*)p + (*i);
 }
-void adiciona(struct pessoa* p, int* countp, int* aux){
-    for(*aux = 0;(*aux) < (*countp);(*aux)++){
+void adiciona(void){
+    for(*aux = 0;(*aux) < (*i);(*aux)++){
         printf("\n\t pessoa %d\n",(*aux) + 1);
         printf("\ndiga o nome: ");
-        scanf("%s",(p + (*aux))->nome);
-        //fgets((p + (*aux))->nome,20,stdin);
+        scanf("%s",p2->nome);
         printf("diga a idade: ");
-        scanf("%d",&(p + (*aux))->idade);
+        scanf("%d",&p2->idade);
         printf("diga o numero de matricula: ");
-        scanf("%d",&(p + (*aux))->mat);
+        scanf("%d",&p2->mat);
+        p2++;
     }
+
 }
-void retira(struct pessoa* p, int* countp, int* aux, int* i){
+void retira(void){
 	for(*aux = 0;(*aux) < (*countp) && (*i) != (p + (*aux))->mat;(*aux)++);
 	for(;(*aux) < (*countp) - 1;(*aux)++){
-			*(p + (*aux)) = *(p + ((*aux) + 1));	
+			*(p + (*aux)) = *(p + ((*aux) + 1));
 	}
 }
-void busca(struct pessoa* p, int* countp, int* aux, int* i){
+void busca(void){
 	for(*aux = 0;(*aux) < (*countp) && (*i) != (p + (*aux))->mat;(*aux)++);
 	if(*aux == *countp){
 		printf("\n\n pessoa nao encontrada!!");
@@ -54,7 +63,7 @@ void busca(struct pessoa* p, int* countp, int* aux, int* i){
 	}
 
 }
-void listar(struct pessoa* p, int* countp, int* aux){
+void listar(void){
 	printf("\n\nlistando..\n");
     for(*aux = 0;(*aux) < (*countp);(*aux)++){
     	printf("\n\t pessoa %d\n",(*aux) + 1);
@@ -66,57 +75,42 @@ void listar(struct pessoa* p, int* countp, int* aux){
 
 
 int main(){
-    void* buffer;
-    struct pessoa* p = NULL;
-    int*aux, *countp, *x, *i;
-	buffer = malloc(4*sizeof(int)+sizeof(struct pessoa));
-    aux = (int*)buffer;
-    countp = (int*)aux + 1;
-    x = (int*)countp + 1;
-    i = (int*)x + 1;
-    p = (struct pessoa*)i + 1;
-    //buffer = alocapont(buffer,p,aux,countp,x);
+    alocapont();
     *countp = 0;
     while((*x) != 5 ){
     	*i = 0;
-    	*x = 0;
     	*aux = 0;
+    	*x = 0;
 		printf("\n\n\tdiga uma opcao: ");
 		scanf("%d",x);
-		
+
     	switch (*x) {
-    
+
         	case 1:
             	printf("diga a quantidade de pessoas que deseja adicionar a agenda: ");
-            	scanf("%d",countp);
-            	realocapont(buffer,p,aux,countp,x,i);
-            	adiciona(p,countp,aux);
+            	scanf("%d",i);
+            	realocapont();
+            	adiciona();
             	break;
         	case 2:
         		printf("diga o numero de matricula da pessoa que deseja remover da agenda: ");
         		scanf("%d",i);
-        		retira(p,countp,aux,i);
-        		//realocapont(buffer,p,aux,countp,x,i);*/
-        	    buffer = realloc(buffer,((*countp)--)*sizeof(struct pessoa));
-    			aux = (int*)buffer;
-    			countp = (int*)aux + 1;
-   				x = (int*)countp + 1;
-   				i = (int*)x + 1;
-    			p = (struct pessoa*)i + 1;
+        		retira();
+        		realocapont();
             	break;
         	case 3:
         		printf("diga o numero de matricula da pessoa desejada: ");
         		scanf("%d",i);
-        		busca(p,countp,aux,i);
+        		busca();
             	break;
         	case 4:
-        		listar(p,countp,aux);
+        		listar();
             	break;
         	case 5:
             break;
     	}
     }
-    
+
     free(buffer);
 
     return 0;
